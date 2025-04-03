54e838b00f028577625df5a32a170635332d6e7e
billdeptrai0512 <billnguyen05121998@gmail.com>
2025-04-03 13:14:04 +0700

    yea

--- src/mainpage/Body/Body.test.jsx
+++ src/mainpage/Body/Body.test.jsx
@@ -2,7 +2,7 @@ import { describe, it, expect } from 'vitest';
 import { render, screen } from '@testing-library/react';
 import userEvent from "@testing-library/user-event";
 
-import Body from './Body';
+import Body from './body';
 
 describe("Body component", () => {
 
