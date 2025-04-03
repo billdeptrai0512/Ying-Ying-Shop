5d04a0ac0addecc64277766cb35d2fee64dc34c4
billdeptrai0512 <billnguyen05121998@gmail.com>
2025-04-03 21:08:15 +0700

    fix header

--- src/mainpage/MainPage.jsx
+++ src/mainpage/MainPage.jsx
@@ -1,5 +1,5 @@
 import './MainPage.css'
-import Header from "../header/Header"
+import Header from '../Header/Header'
 import { Outlet } from "react-router-dom"
 
 export default function MainPage() {
