beba8d5ba72160cc6987f1f462eb895e4b62477a
billdeptrai0512 <billnguyen05121998@gmail.com>
2025-04-03 20:16:58 +0700

    header case sensitive

--- src/mainpage/MainPage.jsx
+++ src/mainpage/MainPage.jsx
@@ -1,6 +1,6 @@
 import './MainPage.css'
 import { Outlet } from "react-router-dom"
-import Header from "../header/Header"
+import Header from "../header/Header.jsx"
 
 export default function MainPage() {
 
